7afa0f63f6cd83130d22fb22b6f83d307c7bdf3c
gardening-y <yj4185@duksung.ac.kr>
2024-06-25 13:16:45 +0900

    [init] 스프링 부트 초기 세팅 - #1

---
+++ dateroad-api/src/main/java/org/dateroad/DateroadApplication.java
@@ -0,0 +1,13 @@
+package org.dateroad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DateroadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DateroadApplication.class, args);
+	}
+
+}
