3494497aa3ca58baf06e03313de4bc8a0b672745
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2024-07-04 20:57:59 +0900

    [feat] SecurityConfig 구현 - #5

---
+++ dateroad-api/src/main/java/org/dateroad/auth/config/SecurityConfig.java
@@ -0,0 +1,54 @@
+package org.dateroad.auth.config;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import org.dateroad.auth.exception.ExceptionHandlerFilter;
+import org.dateroad.auth.exception.JwtAuthenticationEntryPoint;
+import org.dateroad.auth.filter.JwtAuthenticationFilter;
+import org.dateroad.auth.jwt.JwtProvider;
+import org.dateroad.auth.jwt.JwtValidator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtValidator jwtValidator;
+    private final JwtProvider jwtProvider;
+
+    private static final String[] whiteList = {"/", "/swagger-ui/**", "/error", "/v3/api-docs/**"};
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(
+                        (sessionManagementConfig) -> sessionManagementConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(
+                        (authorizeRequestConfig) -> authorizeRequestConfig.anyRequest().authenticated())
+                .exceptionHandling(
+                        exceptionHandlingConfigurer -> exceptionHandlingConfigurer.authenticationEntryPoint(jwtAuthenticationEntryPoint))
+                .addFilterBefore(new JwtAuthenticationFilter(jwtValidator, jwtProvider), UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(new ExceptionHandlerFilter(), JwtAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().requestMatchers(whiteList);
+    }
+
+}
