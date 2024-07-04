490c6150cd89cb8a0a48200a1c039555c46be709
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2024-07-04 21:23:57 +0900

    [setting] SwaggerConfig에 Security 추가 - #5

--- dateroad-api/src/main/java/org/dateroad/config/SwaggerConfig.java
+++ dateroad-api/src/main/java/org/dateroad/config/SwaggerConfig.java
@@ -1,10 +1,20 @@
 package org.dateroad.config;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@SecurityScheme(
+        name = "Authorization",
+        type = SecuritySchemeType.HTTP,
+        in = SecuritySchemeIn.HEADER,
+        bearerFormat = "JWT",
+        scheme = "Bearer"
+)
 @Configuration
 public class SwaggerConfig {
 
