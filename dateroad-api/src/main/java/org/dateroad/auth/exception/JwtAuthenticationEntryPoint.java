0b0e7656d1f5ff14487d64ae64a4e88ee78f4402
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2024-07-04 20:57:50 +0900

    [feat] JwtAuthenticationEntryPoint 구현 - #5

---
+++ dateroad-api/src/main/java/org/dateroad/auth/exception/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,40 @@
+package org.dateroad.auth.exception;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.dateroad.code.FailureCode;
+import org.dateroad.common.Constants;
+import org.dateroad.common.FailureResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@Slf4j
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+        handleException(response);
+    }
+
+    private void handleException(HttpServletResponse response) throws IOException {
+        setResponse(response, HttpStatus.UNAUTHORIZED, FailureCode.UNAUTHORIZED);
+    }
+
+    private void setResponse(HttpServletResponse response, HttpStatus httpStatus, FailureCode failureCode) throws IOException {
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding(Constants.CHARACTER_TYPE);
+        response.setStatus(httpStatus.value());
+        PrintWriter writer = response.getWriter();
+        writer.write(objectMapper.writeValueAsString(FailureResponse.of(failureCode)));
+    }
+}
