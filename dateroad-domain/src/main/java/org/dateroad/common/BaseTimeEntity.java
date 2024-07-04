181f3fbd8d6fe45aaa5ce57034028869ceaeda08
rlarlgnszx <rlarlgnszx@naver.com>
2024-07-05 01:13:39 +0900

    [feat] 기본 BaseTimeEntity 구현 - #6

---
+++ dateroad-domain/src/main/java/org/dateroad/common/BaseTimeEntity.java
@@ -0,0 +1,32 @@
+package org.dateroad.common;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@SuperBuilder
+@MappedSuperclass
+public abstract class BaseTimeEntity {
+
+    @Column(updatable = false)
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedAt;
+
+}
