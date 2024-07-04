3f6af05a4683f5799bfe3839be46433d04f8fd19
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2024-07-04 20:56:47 +0900

    [feat] UserAuthentication 구현 - #5

---
+++ dateroad-api/src/main/java/org/dateroad/auth/filter/UserAuthentication.java
@@ -0,0 +1,22 @@
+package org.dateroad.auth.filter;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.Collection;
+
+public class UserAuthentication extends UsernamePasswordAuthenticationToken {
+
+    public UserAuthentication(Object principal, Object credentials) {
+        super(principal, credentials);
+    }
+
+    public UserAuthentication(Object principal, Object credentials, Collection<? extends GrantedAuthority> authorities) {
+        super(principal, credentials, authorities);
+    }
+
+    public static UserAuthentication createUserAuthentication(Long userId) {
+        return new UserAuthentication(userId, null);
+    }
+}
