22fedd989ee9d3d78766b59e821ca0f98f01649c
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2024-07-07 23:31:05 +0900

    [feat] 카카오feignAPI 인터페이스 구현 - #19

---
+++ dateroad-external/src/main/java/org/dateroad/feign/kakao/KakaoFeignApi.java
@@ -0,0 +1,11 @@
+package org.dateroad.feign.kakao;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@FeignClient(name = "${feign.kakao.name}", url = "${feign.kakao.url}")
+public interface KakaoFeignApi {
+    @GetMapping
+    KakaoAccessTokenRes getKakaoPlatformUserId(@RequestHeader("Authorization") String accessTokenWithTokenType);
+}
