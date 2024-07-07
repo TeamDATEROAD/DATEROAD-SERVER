7cf837e729fd9b21b61fe5ddf27099dac9002ffa
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2024-07-08 06:13:15 +0900

    [feat] ApplePublicKeys 구현 - #19

---
+++ dateroad-external/src/main/java/org/dateroad/feign/apple/ApplePublicKeys.java
@@ -0,0 +1,24 @@
+package org.dateroad.feign.apple;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.dateroad.code.FailureCode;
+import org.dateroad.exception.UnauthorizedException;
+
+import java.util.List;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class ApplePublicKeys {
+    private List<ApplePublicKey> keys;
+
+    // alg(알고리즘) 및 kid(키 식별자) 값을 사용하여 일치하는 공개 키 찾기
+    public ApplePublicKey getMatchesKey(String alg, String kid) {
+        return keys.stream()
+                .filter(applePublicKey -> applePublicKey.alg().equals(alg) && applePublicKey.kid().equals(keys))
+                .findFirst()
+                .orElseThrow(() -> new UnauthorizedException(FailureCode.INVALID_APPLE_IDENTITY_TOKEN));
+    }
+}
