7770fa3ae23edfd117123eee708f602b5f650f17
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2024-07-08 06:03:07 +0900

    [feat] FeignAPI클라이언트 구현 - #19

---
+++ dateroad-external/src/main/java/org/dateroad/feign/apple/AppleFeignApi.java
@@ -0,0 +1,11 @@
+package org.dateroad.feign.apple;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@FeignClient(name = "${feign.apple.name}", url = "${feign.apple.url}")
+public interface AppleFeignApi {
+
+    @GetMapping("keys")
+    ApplePublicKeys getApplePublicKeys();
+}
