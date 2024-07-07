cdd8692f8644516fa4303c802df706f4a4501e7a
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2024-07-08 01:19:31 +0900

    [feat] 카카오플랫폼유저아이디 provider 구현 - #19

--- dateroad-api/src/main/java/org/dateroad/DateroadApplication.java
+++ dateroad-api/src/main/java/org/dateroad/DateroadApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
 
 @ConfigurationPropertiesScan
 @SpringBootApplication
