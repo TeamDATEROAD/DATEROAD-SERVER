c096cfbdaf9b6e860c22dbe07f8781410048e9f4
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2024-07-07 17:03:37 +0900

    [feat] 키 생성 메서드 추가 - #17

--- dateroad-api/src/main/java/org/dateroad/auth/jwt/JwtGenerator.java
+++ dateroad-api/src/main/java/org/dateroad/auth/jwt/JwtGenerator.java
@@ -1,20 +1,22 @@
 package org.dateroad.auth.jwt;
 
-import io.jsonwebtoken.Header;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
 import lombok.RequiredArgsConstructor;
 import org.dateroad.code.FailureCode;
 import org.dateroad.exception.EntityNotFoundException;
+import org.dateroad.exception.UnauthorizedException;
 import org.springframework.stereotype.Component;
 
+import java.security.Key;
+import java.util.Base64;
 import java.util.Date;
 
 @RequiredArgsConstructor
 @Component
 public class JwtGenerator {
     private final JwtProperties jwtProperties;
-    private final KeyProvider keyProvider;
 
     public String generateToken(final long userId, final TokenType tokenType) {
         final Date now = new Date();
@@ -25,7 +27,7 @@ public class JwtGenerator {
                 .setSubject(String.valueOf(userId))
                 .setIssuedAt(now)
                 .setExpiration(expiryDate)
-                .signWith(keyProvider.getSigningKey(), SignatureAlgorithm.HS256)
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
 
@@ -37,5 +39,36 @@ public class JwtGenerator {
         } else {
             throw new EntityNotFoundException(FailureCode.TOKEN_TYPE_NOT_FOUND);
         }
+    }
+
+    public Key getSigningKey() {
+        return Keys.hmacShaKeyFor(encodeSecretKeyToBase64().getBytes());
+    }
+
+    private String encodeSecretKeyToBase64() {
+        return Base64.getEncoder().encodeToString(jwtProperties.getSecret().getBytes());
+    }
+
+    public Jws<Claims> parseToken(String token) {
+        try {
+            JwtParser jwtParser = getJwtParser();
+            return jwtParser.parseClaimsJws(token);
+        } catch (ExpiredJwtException e) {
+            throw new UnauthorizedException(FailureCode.EXPIRED_ACCESS_TOKEN);
+        } catch (UnsupportedJwtException e) {
+            throw new UnauthorizedException(FailureCode.UNSUPPORTED_TOKEN_TYPE);
+        } catch (MalformedJwtException e) {
+            throw new UnauthorizedException(FailureCode.MALFORMED_TOKEN);
+        } catch (SignatureException e) {
+            throw new UnauthorizedException(FailureCode.INVALID_SIGNATURE_TOKEN);
+        } catch (Exception e) {
+            throw new UnauthorizedException(FailureCode.INVALID_ACCESS_TOKEN_VALUE);
+        }
+    }
+
+    public JwtParser getJwtParser() {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build();
     }
 }
