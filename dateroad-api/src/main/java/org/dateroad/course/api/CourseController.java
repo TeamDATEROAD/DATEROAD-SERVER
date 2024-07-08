c2657acd9183f87c88eedb6c3f90a4ee4ff24709
rlarlgnszx <rlarlgnszx@naver.com>
2024-07-08 16:06:39 +0900

    [feat] CourseController 구현 - #22

---
+++ dateroad-api/src/main/java/org/dateroad/course/api/CourseController.java
@@ -0,0 +1,31 @@
+package org.dateroad.course.api;
+
+
+import lombok.RequiredArgsConstructor;
+import org.dateroad.course.dto.request.CourseGetAllReq;
+import org.dateroad.course.dto.response.CourseGetAllRes;
+import org.dateroad.course.service.CourseService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+///api/v1/courses : url change
+@RequiredArgsConstructor
+public class CourseController {
+    private final CourseService courseService;
+
+    @GetMapping
+    public ResponseEntity<CourseGetAllRes> getAllCourse(
+            @ModelAttribute CourseGetAllReq courseGetAllReq
+    ) {
+        CourseGetAllRes courseAll = courseService.getAllCourses(courseGetAllReq);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(courseAll);
+    }
+}
