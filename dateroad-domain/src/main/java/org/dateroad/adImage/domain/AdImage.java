702f003924a4bc9497387590210ff6e358b4f645
rlarlgnszx <rlarlgnszx@naver.com>
2024-07-08 22:09:28 +0900

    [feat] 광고 상세 Image저장 엔티티 및 Repository 구현 - #29

---
+++ dateroad-domain/src/main/java/org/dateroad/adImage/domain/AdImage.java
@@ -0,0 +1,54 @@
+package org.dateroad.adImage.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.dateroad.advertisement.domain.Advertisment;
+import org.dateroad.common.BaseTimeEntity;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder(access = AccessLevel.PRIVATE)
+@Table(name = "ad_images")
+public class AdImage extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "image_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "advertisment_id")
+    @NotNull
+    private Advertisment advertisment;
+
+    @Column(name = "image_url")
+    @NotNull
+    @Getter
+    private String imageUrl;
+
+    @Column(name = "sequence")
+    @NotNull
+    private int sequence;
+
+    public static AdImage create(final Advertisment advertisment, final String imageUrl, int sequence) {
+        return AdImage.builder()
+                .advertisment(advertisment)
+                .imageUrl(imageUrl)
+                .sequence(sequence)
+                .build();
+    }
+}
