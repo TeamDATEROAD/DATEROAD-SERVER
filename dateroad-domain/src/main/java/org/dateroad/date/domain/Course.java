313dba7d8f58031e07acfd036198aa6c5e991068
rlarlgnszx <rlarlgnszx@naver.com>
2024-07-08 16:07:43 +0900

    [chore] Course entity에 Getter 추가 - #22

--- dateroad-domain/src/main/java/org/dateroad/date/domain/Course.java
+++ dateroad-domain/src/main/java/org/dateroad/date/domain/Course.java
@@ -12,11 +12,13 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
+@Getter
 @SuperBuilder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
