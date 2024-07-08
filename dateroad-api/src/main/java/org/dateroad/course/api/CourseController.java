ab466093ece4b766848b71f22f7dbca463532f2c
rlarlgnszx <rlarlgnszx@naver.com>
2024-07-08 17:01:52 +0900

    [feat] RequestMapping Url 변경 - #22

--- dateroad-api/src/main/java/org/dateroad/course/api/CourseController.java
+++ dateroad-api/src/main/java/org/dateroad/course/api/CourseController.java
@@ -13,8 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
-///api/v1/courses : url change
+@RequestMapping("/api/v1/courses")
 @RequiredArgsConstructor
 public class CourseController {
     private final CourseService courseService;
