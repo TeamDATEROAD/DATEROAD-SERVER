3f550f0bd4751d9a16f974826bb517693b0abaf2
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2024-07-10 08:20:05 +0900

    [feat] 애플 FeginProvider 회원탈퇴 구현 및 이름 변경 - #50

--- dateroad-external/src/main/java/org/dateroad/feign/apple/AppleFeignProvider.java
+++ dateroad-external/src/main/java/org/dateroad/feign/apple/AppleFeignProvider.java
@@ -6,16 +6,19 @@ import org.dateroad.code.FailureCode;
 import org.dateroad.exception.UnauthorizedException;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.security.PublicKey;
 import java.util.Map;
 
 @RequiredArgsConstructor
 @Component
-public class ApplePlatformUserIdProvider {
+public class AppleFeignProvider {
     private final AppleIdentityJWTParser appleIdentityJWTParser;
     private final AppleFeignApi appleFeignApi;
     private final AppleClientPublicKeyGenerator appleClientPublicKeyGenerator;
     private final AppleIdentityJWTValidator appleIdentityJWTValidator;
+    private final AppleClientSecretGenerator appleClientSecretGenerator;
+    private final AppleProperties appleProperties;
 
     public String getApplePlatformUserId(final String identityToken) {
         Map<String, String> tokenHeaders = appleIdentityJWTParser.parseHeader(identityToken);
@@ -26,9 +29,20 @@ public class ApplePlatformUserIdProvider {
         return claims.getSubject();
     }
 
+    public void revokeUser(final String authCode) {
+        try {
+            String secretKey = appleClientSecretGenerator.createClientSecret();
+            String appleAccessToken = appleClientSecretGenerator.getAppleAccessToken(secretKey, authCode);
+            appleFeignApi.revokeUser(secretKey, appleAccessToken, appleProperties.getClientId());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     private void validateClaims(final Claims claims) {
         if (!appleIdentityJWTValidator.isValidAppleIdentityToken(claims)) {
             throw new UnauthorizedException(FailureCode.INVALID_APPLE_IDENTITY_TOKEN_CLAIMS);
         }
     }
+
 }
