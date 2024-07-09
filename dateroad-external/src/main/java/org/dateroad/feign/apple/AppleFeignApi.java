cd77dcc2ac3eb400c730b3cc4abebcbc1d68af02
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2024-07-10 08:19:44 +0900

    [feat] 애플FeginApi 애플토큰 가져오기, 회원탈퇴 구현 - #50

--- dateroad-external/src/main/java/org/dateroad/feign/apple/AppleFeignApi.java
+++ dateroad-external/src/main/java/org/dateroad/feign/apple/AppleFeignApi.java
@@ -2,10 +2,23 @@ package org.dateroad.feign.apple;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "${feign.apple.name}", url = "${feign.apple.url}")
 public interface AppleFeignApi {
 
     @GetMapping("/keys")
     ApplePublicKeys getApplePublicKeys();
+
+    @PostMapping("/token")
+    AppleTokenRes getAppleToken(@RequestParam("client_secret") String clientSecret,
+                         @RequestParam("code") String authCode,
+                         @RequestParam("grant_type") String grantType,
+                         @RequestParam("client_id") String clientId);
+
+    @PostMapping("/revoke")
+    void revokeUser(@RequestParam("client_secret") String clientSecret,
+                    @RequestParam("accessToken") String accessToken,
+                    @RequestParam("client_id") String clientId);
 }
