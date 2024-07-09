a5bfdc0505594c9a93e13d7160af6efc33b15997
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2024-07-09 17:17:36 +0900

    [feat] userRepository에 닉네임으로 존재하는지 판별하는 메서드 구현 - #40

--- dateroad-domain/src/main/java/org/dateroad/user/repository/UserRepository.java
+++ dateroad-domain/src/main/java/org/dateroad/user/repository/UserRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsUserByPlatFormAndPlatformUserId(final Platform platform, final String platformUserId);
+
+    boolean existsByName(final String name);
 }
