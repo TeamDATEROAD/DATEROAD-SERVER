2f9d97685547702d7deae5ee80131bbc97d1f0c5
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2024-07-10 08:18:22 +0900

    [feat] 카카오FeginApi에 회원탈퇴 추가 - #50

--- dateroad-external/src/main/java/org/dateroad/feign/kakao/KakaoFeignApi.java
+++ dateroad-external/src/main/java/org/dateroad/feign/kakao/KakaoFeignApi.java
@@ -1,13 +1,9 @@
 package org.dateroad.feign.kakao;
 
-import org.dateroad.feign.kakao.dto.request.KakaoUnlinkReq;
 import org.dateroad.feign.kakao.dto.response.KaKaoUnlinkRes;
 import org.dateroad.feign.kakao.dto.response.KakaoAccessTokenInfoRes;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.*;
 
 @FeignClient(name = "${feign.kakao.name}", url = "${feign.kakao.url}")
 public interface KakaoFeignApi {
@@ -15,7 +11,7 @@ public interface KakaoFeignApi {
     KakaoAccessTokenInfoRes getKakaoPlatformUserId(@RequestHeader("Authorization") String accessTokenWithTokenType);
 
     @PostMapping("/unlink")
-    void unlink(@RequestHeader("Authorization") String appAdminKey,
-                          @RequestBody KakaoUnlinkReq kakaoUnlinkReq);
-
+    KaKaoUnlinkRes unlink(@RequestHeader("Authorization") String appAdminKey,
+                          @RequestParam("target_id_type") String targetIdType,
+                          @RequestParam("target_id") Long tagerId);
 }
