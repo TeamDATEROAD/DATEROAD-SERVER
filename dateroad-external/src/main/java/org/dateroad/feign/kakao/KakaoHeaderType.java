29227d9cc49e45cd35142b037c3382c6b50799b8
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2024-07-10 03:55:43 +0900

    [feat] 카카오 연결 해제 메서드 추가 및 헤더생성 메서드 변경 - #50

---
+++ dateroad-external/src/main/java/org/dateroad/feign/kakao/KakaoHeaderType.java
@@ -0,0 +1,14 @@
+package org.dateroad.feign.kakao;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum KakaoHeaderType {
+    BEARER("Bearer "),
+    KAKAOAK("KakaoAK "),
+    ;
+
+    private final String tokenType;
+}
