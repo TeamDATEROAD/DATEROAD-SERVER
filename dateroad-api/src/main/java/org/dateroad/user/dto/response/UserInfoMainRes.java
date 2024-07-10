675819d7f41bda613c464c3bbbba5651371101b9
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2024-07-11 01:38:25 +0900

    [feat] 유저 서비스에 회원정보조회(메인) 구현 - #57

---
+++ dateroad-api/src/main/java/org/dateroad/user/dto/response/UserInfoMainRes.java
@@ -0,0 +1,8 @@
+package org.dateroad.user.dto.response;
+
+public record UserProfileMainRes(
+        String name,
+        int point,
+        String imageUrl
+) {
+}
---
+++ dateroad-api/src/main/java/org/dateroad/user/service/UserService.java
@@ -0,0 +1,28 @@
+package org.dateroad.user.service;
+
+import lombok.RequiredArgsConstructor;
+import org.dateroad.code.FailureCode;
+import org.dateroad.exception.EntityNotFoundException;
+import org.dateroad.user.domain.User;
+import org.dateroad.user.dto.response.UserInfoMainRes;
+import org.dateroad.user.repository.UserRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserInfoMainRes getUserInfoMain(final Long userId) {
+        User foundUser = findUserById(userId);
+        return UserInfoMainRes.of(foundUser.getName(), foundUser.getTotalPoint(), foundUser.getImageUrl());
+    }
+
+    private User findUserById(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new EntityNotFoundException(FailureCode.USER_NOT_FOUND));
+    }
+
+}
