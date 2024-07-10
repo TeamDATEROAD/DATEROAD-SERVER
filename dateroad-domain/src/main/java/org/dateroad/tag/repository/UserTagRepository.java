a4f0ee398c286a9f8f2136b1494f7abcf6f6698a
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2024-07-11 07:54:23 +0900

    [feat] 유저아이디로 UserTagList 가져오기 - #61

--- dateroad-domain/src/main/java/org/dateroad/tag/repository/UserTagRepository.java
+++ dateroad-domain/src/main/java/org/dateroad/tag/repository/UserTagRepository.java
@@ -1,8 +1,12 @@
 package org.dateroad.tag.repository;
 
+import org.dateroad.tag.domain.DateTagType;
 import org.dateroad.tag.domain.UserTag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 public interface UserTagRepository extends JpaRepository<UserTag, Long> {
+    List<UserTag> findAllByUserId(Long userId);
 }
