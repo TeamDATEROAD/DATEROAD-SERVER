de93737086f6c809bd2e37bedc3d191228339d3a
rlarlgnszx <rlarlgnszx@naver.com>
2024-07-12 08:10:09 +0900

    [feat] 광고 조회 관련 DTO 구현 - #71

---
+++ dateroad-api/src/main/java/org/dateroad/advertisment/dto/response/AdvGetDetailRes.java
@@ -0,0 +1,40 @@
+package org.dateroad.advertisment.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.Builder;
+
+@Builder(access = AccessLevel.PROTECTED)
+public record AdvGetDetailRes(
+        List<AdvImagesRes> images,
+        String title,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd", timezone = "Asia/Seoul")
+
+        LocalDate createAt,
+        String description
+) {
+    public static AdvGetDetailRes of(List<AdvImagesRes> images, String title, LocalDate createAt, String description) {
+        return AdvGetDetailRes.builder()
+                .images(images)
+                .title(title)
+                .createAt(createAt)
+                .description(description)
+                .build();
+    }
+
+    @Builder
+    public record AdvImagesRes(
+            String imagesUrl,
+            int sequence
+    ) {
+        public static AdvImagesRes of(String imagesUrl, int sequence) {
+            return AdvImagesRes.builder()
+                    .imagesUrl(imagesUrl)
+                    .sequence(sequence)
+                    .build();
+        }
+    }
+}
