34173e7763f26b0d927235be007ab7e356d6a000
rlarlgnszx <rlarlgnszx@naver.com>
2024-07-12 08:09:52 +0900

    [feat] 광고 상세조회 구현 - #71

--- dateroad-api/src/main/java/org/dateroad/advertisment/api/AdvertismentController.java
+++ dateroad-api/src/main/java/org/dateroad/advertisment/api/AdvertismentController.java
@@ -2,10 +2,13 @@ package org.dateroad.advertisment.api;
 
 import lombok.RequiredArgsConstructor;
 import org.dateroad.advertisment.dto.response.AdvGetAllRes;
+import org.dateroad.advertisment.dto.response.AdvGetDetailRes;
 import org.dateroad.advertisment.service.AdvertismentService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +23,14 @@ public class AdvertismentController {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(advertismentService.getAllAdvertisments());
+    }
+
+    @GetMapping("{advId}")
+    public ResponseEntity<AdvGetDetailRes> getAllAdvertisments(
+            final @PathVariable Long advId
+    ){
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(advertismentService.getAdvertismentsDetail(advId));
     }
 }
