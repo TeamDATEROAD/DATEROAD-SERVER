7ec15134a04419edf8476ae93785da1fa50a62b1
rlarlgnszx <rlarlgnszx@naver.com>
2024-07-11 18:52:05 +0900

    [fix] repository 어노테이션 추가 - #63

--- dateroad-domain/src/main/java/org/dateroad/dateAccess/repository/DataAccessRepository.java
+++ dateroad-domain/src/main/java/org/dateroad/dateAccess/repository/DataAccessRepository.java
@@ -6,7 +6,9 @@ import org.dateroad.dateAccess.domain.DateAccess;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface DataAccessRepository extends JpaRepository<DateAccess,Long> {
     @Query("SELECT da.course FROM DateAccess da WHERE da.user.id = :userId")
     List<Course> findCoursesByUserId(@Param("userId") Long userId);
