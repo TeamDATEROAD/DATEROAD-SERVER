69271dba6a2ba4f840b8f0aca2f14261c954f6b9
gardening-y <yj4185@duksung.ac.kr>
2024-07-12 22:08:56 +0900

    [fix] import 인식 오류 해결 - #85

--- dateroad-domain/src/main/java/org/dateroad/date/repository/CourseRepository.java
+++ dateroad-domain/src/main/java/org/dateroad/date/repository/CourseRepository.java
@@ -7,6 +7,7 @@ import org.dateroad.user.domain.User;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
