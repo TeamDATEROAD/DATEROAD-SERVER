cd6ed2ed33c0d623216436390e7182ff1e4b50f1
gardening-y <yj4185@duksung.ac.kr>
2024-07-12 10:08:45 +0900

    [feat] getMyCourses Repository 구현 - #80

--- dateroad-domain/src/main/java/org/dateroad/date/repository/CourseRepository.java
+++ dateroad-domain/src/main/java/org/dateroad/date/repository/CourseRepository.java
@@ -1,18 +1,16 @@
 package org.dateroad.date.repository;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.dateroad.date.domain.Course;
+import org.dateroad.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-// CourseRepository.java
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> , JpaSpecificationExecutor<Course> {
     boolean existsByUserId(Long userId);
-
+    List<Course> findByUser(User user);
 }
 
