22ae54e339db2167a8903406f3b7678e54dc5210
gardening-y <yj4185@duksung.ac.kr>
2024-07-12 09:31:48 +0900

    [feat] getSortedCourses 인기순, 최신순 Repository Query 구현 - #79

--- dateroad-domain/src/main/java/org/dateroad/date/repository/CourseRepository.java
+++ dateroad-domain/src/main/java/org/dateroad/date/repository/CourseRepository.java
@@ -1,18 +1,20 @@
 package org.dateroad.date.repository;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.dateroad.date.domain.Course;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-// CourseRepository.java
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> , JpaSpecificationExecutor<Course> {
     boolean existsByUserId(Long userId);
-
+    @Query("SELECT c FROM Course c LEFT JOIN Like l ON c.id = l.course.id GROUP BY c.id ORDER BY COUNT(l) DESC")
+    List<Course> findTopCoursesByLikes(Pageable pageable);
+    @Query("SELECT c FROM Course c ORDER BY c.createdAt DESC")
+    List<Course> findTopCoursesByCreatedAt(Pageable pageable);
 }
 
