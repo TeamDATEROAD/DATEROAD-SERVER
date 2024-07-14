7b1cce87065832868b7b18b3f3a34f88a5d2ff02
rlarlgnszx <rlarlgnszx@naver.com>
2024-07-14 22:42:37 +0900

    [docs] 광고 swagger 작성-  #94

---
+++ dateroad-api/src/main/java/org/dateroad/advertisment/api/AdvertismentApi.java
@@ -0,0 +1,94 @@
+package org.dateroad.advertisment.api;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.dateroad.advertisment.dto.response.AdvGetAllRes;
+import org.dateroad.advertisment.dto.response.AdvGetDetailRes;
+import org.dateroad.auth.argumentresolve.UserId;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Tag(name = "광고 관련 API")
+@SecurityRequirement(name = "Authorization")
+public interface AdvertismentApi {
+    @Operation(
+            summary = "광고 전체 조회 API",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = AdvGetAllRes.class)),
+                            description = "요청이 성공했습니다."),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "잘못된 요청입니다.",
+                            content = @Content),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "액세스 토큰의 형식이 올바르지 않습니다. Bearer 타입을 확인해 주세요.",
+                            content = @Content),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "액세스 토큰의 값이 올바르지 않습니다.",
+                            content = @Content),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "액세스 토큰이 만료되었습니다. 재발급 받아주세요.",
+                            content = @Content),
+                    @ApiResponse(
+                            responseCode = "405",
+                            description = "잘못된 HTTP method 요청입니다.",
+                            content = @Content),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "서버 내부 오류입니다.",
+                            content = @Content)})
+    ResponseEntity<AdvGetAllRes> getAllAdvertisments();
+    @Operation(
+            summary = "광고 상세 조회 API",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "요청이 성공했습니다.",
+                            content = @Content(schema = @Schema(implementation = AdvGetDetailRes.class))),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "잘못된 요청입니다.",
+                            content = @Content),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "유효하지 않은 요청 파라미터 값입니다.",
+                            content = @Content),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "액세스 토큰의 형식이 올바르지 않습니다. Bearer 타입을 확인해 주세요.",
+                            content = @Content),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "액세스 토큰의 값이 올바르지 않습니다.",
+                            content = @Content),
+                    @ApiResponse(
+                            responseCode = "401",
+                            description = "액세스 토큰이 만료되었습니다. 재발급 받아주세요.",
+                            content = @Content),
+
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "존재하지 않는 광고입니다.",
+                            content = @Content),
+                    @ApiResponse(
+                            responseCode = "405",
+                            description = "잘못된 HTTP method 요청입니다.",
+                            content = @Content),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "서버 내부 오류입니다.",
+                            content = @Content)})
+    ResponseEntity<AdvGetDetailRes> getAdvertismentsDetail(
+            @Parameter(hidden = true) final @PathVariable Long advId
+    );
+}
