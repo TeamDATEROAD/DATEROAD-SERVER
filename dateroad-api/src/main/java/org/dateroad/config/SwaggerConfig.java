28bf4c8f541da11116b9c80f424bee5e90a35ccb
rlarlgnszx <rlarlgnszx@naver.com>
2024-07-16 20:24:52 +0900

    [feat] swagger 명시타입 추가 - #117

--- dateroad-api/src/main/java/org/dateroad/config/SwaggerConfig.java
+++ dateroad-api/src/main/java/org/dateroad/config/SwaggerConfig.java
@@ -5,8 +5,11 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import java.util.ArrayList;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 
 @SecurityScheme(
         name = "Authorization",
@@ -29,5 +32,13 @@ public class SwaggerConfig {
                 .title("DateRoad API Docs")
                 .description("34기 앱잼팀 'DateRoad' API 명세서입니다.")
                 .version("v1.0");
+    }
+
+    public SwaggerConfig(MappingJackson2HttpMessageConverter converter) {
+        var supportedMediaTypes = new ArrayList<>(converter.getSupportedMediaTypes());
+        supportedMediaTypes.add(new MediaType("application", "json"));
+        supportedMediaTypes.add(new MediaType("multipart", "form-data"));
+        supportedMediaTypes.add(new MediaType("application", "octet-stream"));
+        converter.setSupportedMediaTypes(supportedMediaTypes);
     }
 }
