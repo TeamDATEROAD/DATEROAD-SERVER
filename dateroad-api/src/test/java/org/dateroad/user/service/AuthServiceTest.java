6201c1ddf2bf2fb87ee3495d6899129b25230a74
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2024-07-18 08:46:19 +0900

    [chore] 스웨거 받아오는 토큰명 변경 - #161

---
+++ dateroad-api/src/test/java/org/dateroad/user/service/AuthServiceTest.java
@@ -0,0 +1,26 @@
+package org.dateroad.user.service;
+
+import org.assertj.core.api.Assertions;
+import org.dateroad.code.FailureCode;
+import org.dateroad.common.ValidatorUtil;
+import org.dateroad.exception.InvalidValueException;
+import org.dateroad.tag.domain.DateTagType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthServiceTest {
+
+    @Mock
+
+
+    @Test
+    @DisplayName("유저가 회원가입을 한다.")
+
+}
