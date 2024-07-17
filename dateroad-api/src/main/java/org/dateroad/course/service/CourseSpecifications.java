2a752998279014421fb4d43818e264b42e4616f6
rlarlgnszx <rlarlgnszx@naver.com>
2024-07-18 02:45:44 +0900

    [fix] 조회 필터링로직 수정 - #158

--- dateroad-api/src/main/java/org/dateroad/course/service/CourseSpecifications.java
+++ dateroad-api/src/main/java/org/dateroad/course/service/CourseSpecifications.java
@@ -6,20 +6,29 @@ import jakarta.persistence.criteria.Predicate;
 import jakarta.persistence.criteria.Root;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.function.BiFunction;
 import org.dateroad.course.dto.request.CourseGetAllReq;
 import org.dateroad.date.domain.Course;
+import org.dateroad.date.domain.Region;
 import org.springframework.data.jpa.domain.Specification;
 
 public class CourseSpecifications {
     public static Specification<Course> filterByCriteria(CourseGetAllReq courseGetAllReq) {
         return (root, query, criteriaBuilder) -> {
             List<Predicate> predicates = new ArrayList<>();
-            addPredicate(predicates, criteriaBuilder, root, "city", courseGetAllReq.city(), criteriaBuilder::equal);
+            addPredicate(predicates, criteriaBuilder, root, "city", courseGetAllReq.city(),
+                    (path, value) -> {
+                        if (value == Region.SubRegion.SEOUL_ENTIRE || value == Region.SubRegion.GYEONGGI_ENTIRE || value == Region.SubRegion.INCHEON_ENTIRE) {
+                            return null;
+                        }
+                        return criteriaBuilder.equal(path, value);
+                    });
             addPredicate(predicates, criteriaBuilder, root, "country", courseGetAllReq.country(),
                     criteriaBuilder::equal);
             addCostPredicate(predicates, criteriaBuilder, root, courseGetAllReq.cost());
+            predicates.removeIf(Objects::isNull);
             return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
         };
     }
@@ -28,9 +37,12 @@ public class CourseSpecifications {
                                          String attributeName, T value,
                                          BiFunction<Path<T>, T, Predicate> predicateFunction) {
         Optional.ofNullable(value)
-                .ifPresent(val -> predicates.add(
-                        predicateFunction.apply(root.get(attributeName), val))
-                );
+                .ifPresent(val -> {
+                    Predicate predicate = predicateFunction.apply(root.get(attributeName), val);
+                    if (predicate != null) {
+                        predicates.add(predicate);
+                    }
+                });
     }
 
     private static void addCostPredicate(List<Predicate> predicates, CriteriaBuilder criteriaBuilder, Root<?> root,
@@ -50,10 +62,10 @@ public class CourseSpecifications {
                     predicates.add(criteriaBuilder.greaterThan(root.get("cost"), 100000));
                     break;
                 default:
+                    predicates.add(criteriaBuilder.lessThanOrEqualTo(root.get("cost"), cost));
                     break;
             }
         }
     }
-
 }
 
