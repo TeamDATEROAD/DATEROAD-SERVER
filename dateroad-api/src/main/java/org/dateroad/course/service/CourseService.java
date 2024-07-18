0c286e75265c0cfc0914aa307d7dcdd881c4ce91
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2024-07-18 09:57:53 +0900

    [feat] 코스생성시 isAccess = true로 변경 - #167

--- dateroad-api/src/main/java/org/dateroad/course/service/CourseService.java
+++ dateroad-api/src/main/java/org/dateroad/course/service/CourseService.java
@@ -178,6 +178,8 @@ public class CourseService {
                 totalTime
         );
         Course saveCourse = courseRepository.save(course);
+        DateAccess dateAccess = DateAccess.create(saveCourse, user);
+        dateAccessRepository.save(dateAccess);
         List<Image> imageList = asyncService.createImage(images, saveCourse);
         String thumbnailUrl = imageList.getFirst().getImageUrl();
         course.setThumbnail(thumbnailUrl);
