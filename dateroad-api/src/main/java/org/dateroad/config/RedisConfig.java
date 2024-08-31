b0b94ba77860be704da060012262433836cb2bb0
rlarlgnszx <rlarlgnszx@naver.com>
2024-08-31 17:35:55 +0900

    [fix] IP 변경 - #235

--- dateroad-api/src/main/java/org/dateroad/config/RedisConfig.java
+++ dateroad-api/src/main/java/org/dateroad/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package org.dateroad.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisClusterConfiguration;
@@ -11,15 +12,18 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
+    @Value("${aws.ip}")
+    private String host;
+
     @Bean
     public RedisConnectionFactory redisConnectionFactoryForCluster() {
         RedisClusterConfiguration clusterConfig = new RedisClusterConfiguration()
-                .clusterNode("127.0.0.1", 7000)
-                .clusterNode("127.0.0.1", 7001)
-                .clusterNode("127.0.0.1", 7002)
-                .clusterNode("127.0.0.1", 7003)
-                .clusterNode("127.0.0.1", 7004)
-                .clusterNode("127.0.0.1", 7005);
+                .clusterNode(host, 7000)
+                .clusterNode(host, 7001)
+                .clusterNode(host, 7002)
+                .clusterNode(host, 7003)
+                .clusterNode(host, 7004)
+                .clusterNode(host, 7005);
         // 클러스터 모드로 LettuceConnectionFactory 설정
         LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(clusterConfig);
         lettuceConnectionFactory.setShareNativeConnection(false); // 클러스터 모드에서 필요에 따라 사용할 수 있음
