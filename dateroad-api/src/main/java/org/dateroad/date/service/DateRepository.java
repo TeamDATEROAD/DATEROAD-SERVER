4835f98edeebe0de52fc0b22ad1ee3023ef7c8b7
rlarlgnszx <rlarlgnszx@naver.com>
2024-09-23 19:51:27 +0900

    [fix] date 지난일정 수정 - #282

--- dateroad-api/src/main/java/org/dateroad/date/service/DateRepository.java
+++ dateroad-api/src/main/java/org/dateroad/date/service/DateRepository.java
@@ -20,19 +20,29 @@ public interface DateRepository extends JpaRepository<Date, Long> {
     @Query(value = "SELECT * FROM dates d " +
             "WHERE d.user_id = :userId " +
             "AND (d.date > :currentDate " +
-            "     OR (d.date = :currentDate AND d.start_at > :currentTime)) " +
-            "ORDER BY d.date ASC, d.start_at ASC " +
+            "     OR (d.date = :currentDate AND d.start_at >= :currentTime)) " +
+            "ORDER BY d.date, d.start_at " +
             "LIMIT 1", nativeQuery = true)
     Optional<Date> findClosestDateByUserIdAndCurrentDate(
             @Param("userId") Long userId,
             @Param("currentDate") LocalDate currentDate,
             @Param("currentTime") LocalTime currentTime);
 
-    @Query("select d from Date d where d.user.id = :userId and d.date < :currentDate order by d.date desc, d.startAt asc")
-    List<Date> findPastDatesByUserId(@Param("userId") Long userId, @Param("currentDate") LocalDate currentDate);
+    @Query("select d from Date d " +
+            "where d.user.id = :userId " +
+            "and (d.date < :currentDate " +
+            "     or (d.date = :currentDate and d.startAt < :currentTime)) " +
+            "order by d.date desc, d.startAt asc")
+    List<Date> findPastDatesByUserId(@Param("userId") Long userId, @Param("currentDate") LocalDate currentDate,
+                                     LocalTime currentTime);
 
-    @Query("select d from Date d where d.user.id = :userId and d.date >= :currentDate order by d.date asc, d.startAt asc")
-    List<Date> findFutureDatesByUserId(@Param("userId") Long userId, @Param("currentDate") LocalDate currentDate);
+    @Query("select d from Date d " +
+            "where d.user.id = :userId " +
+            "and (d.date > :currentDate " +
+            "     or (d.date = :currentDate and d.startAt > :currentTime)) " +
+            "order by d.date asc, d.startAt asc")
+    List<Date> findFutureDatesByUserId(@Param("userId") Long userId, @Param("currentDate") LocalDate currentDate,
+                                       LocalTime currentTime);
 
     @Modifying
     @Query("DELETE FROM UserTag ut WHERE ut.user.id = :userId")
