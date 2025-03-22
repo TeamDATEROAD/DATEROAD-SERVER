dbd2e03e7fd197a909473d519d5432fdeb81a4a1
rlarlgnszx <rlarlgnszx@naver.com>
2025-03-22 15:33:29 +0900

    fix: jpa query

--- dateroad-domain/src/main/java/org/dateroad/admin/repository/WarningRepository.java
+++ dateroad-domain/src/main/java/org/dateroad/admin/repository/WarningRepository.java
@@ -9,6 +9,6 @@ import java.util.List;
 
 @Repository
 public interface WarningRepository extends JpaRepository<Warning, Long> {
-    List<Warning> findAllByUserAndIsActiveTrue(User user);
+    List<Warning> findAllByUserAndActiveIsTrue(User user);
     long countByUserAndActiveTrue(User user);
 }
