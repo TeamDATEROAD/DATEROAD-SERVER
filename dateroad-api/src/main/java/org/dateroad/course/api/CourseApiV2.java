6dff8ee866eb13c499af1dab84fe87c95404a21a
gardening-y <yj4185@duksung.ac.kr>
2025-03-31 10:07:29 +0900

    [feat] Course create Controller v2 변경 - #352

--- dateroad-api/src/main/java/org/dateroad/course/api/CourseApiV2.java
+++ dateroad-api/src/main/java/org/dateroad/course/api/CourseApiV2.java
@@ -11,10 +11,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Size;
 import org.dateroad.auth.argumentresolve.UserId;
-import org.dateroad.course.dto.request.CourseCreateReq;
-import org.dateroad.course.dto.request.CourseCreateSwaggerDto;
-import org.dateroad.course.dto.request.CoursePlaceGetReqV2;
-import org.dateroad.course.dto.request.TagCreateReq;
+import org.dateroad.course.dto.request.*;
 import org.dateroad.course.dto.response.CourseCreateRes;
 import org.dateroad.date.dto.response.CourseGetDetailResV2;
 import org.springframework.http.ResponseEntity;
@@ -40,7 +37,7 @@ public interface CourseApiV2 {
     )
     ResponseEntity<CourseCreateRes> createCourse(
             @UserId final Long userId,
-            @RequestPart("course") @Valid final CourseCreateReq courseCreateReq,
+            @RequestPart("course") @Valid final CourseCreateReqV2 courseCreateReq,
             @RequestPart("tags") @Validated @Size(min = 1, max = 3) final List<TagCreateReq> tags,
             @RequestPart("places") @Validated @Size(min = 1) final List<CoursePlaceGetReqV2> places,
             @RequestPart("images") @Validated @Size(min =1, max = 10) final List<MultipartFile> images
